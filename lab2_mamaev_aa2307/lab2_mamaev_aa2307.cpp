1a026daadf9a0d53932a6efb29090ac37a953872
Mamaev <rostislavmamaev@yandex.ru>
2024-10-29 00:07:09 +0300

    begin

---
+++ lab2_mamaev_aa2307/lab2_mamaev_aa2307.cpp
@@ -0,0 +1,22 @@
+﻿//﻿#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+T& SelectElement(unordered_map <int, T>& elements)
+{
+	int Id;
+	cout << "Enter number: ";
+	InputCorrectNumber(Id);
+	while (elements.find(Id) == elements.end())
+	{
+		cout << "Error!\nThe number exceeds the number of elements." << endl
+			<< "Number of elements: " << elements.size() << endl
+			<< "Please enter correct data: ";
+		InputCorrectNumber(Id);
+	}
+	return elements[Id];
+}
